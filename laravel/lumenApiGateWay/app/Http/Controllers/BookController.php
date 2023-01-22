30cc9984de400dd2b8d3b941cfadb85296bb10e8
mwachandea <mwachandea@gmail.com>
2023-01-22 13:53:49 +0200

    Editing ApiGateway

--- laravel/lumenApiGateWay/app/Http/Controllers/BookController.php
+++ laravel/lumenApiGateWay/app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Services\AuthorService;
 use App\Services\BookService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
@@ -18,14 +19,22 @@ class BookController extends Controller
      * @var $BookService
      */
     public $bookService;
+
+    /**
+     * [The service to consume the authors microservice]
+     *
+     * @var $BookService
+     */
+    public $authorService;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(BookService $bookService)
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
         $this->bookService = $bookService;
+        $this->authorService = $authorService;
     }
 
     /**
@@ -43,6 +52,8 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
+        $this->authorService->obtainAuthor($request->author_id);
+
         return $this->successResponse($this->bookService->createBook($request->all(), Response::HTTP_CREATED));
     }
 
